e7f86f193a18d5bc05f8d1eddcde38ac8dd9d099
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-04-27 23:39:11 +0300

    fix: task3 upper case for type

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -14,7 +14,7 @@ export default function TransactionHistory({ items }) {
       <tbody>
         {items.map(({ id, type, amount, currency }) => (
           <tr key={id}>
-            <td>{type.charAt(0).toUpperCase()}</td>
+            <td>{type.charAt(0).toUpperCase() + type.slice(1)}</td>
             <td>{amount}</td>
             <td>{currency}</td>
           </tr>
